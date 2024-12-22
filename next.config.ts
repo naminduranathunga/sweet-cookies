b35eabdba6c4a8a8b7abb556e7dbe5b4a1a4f500
Namindu <namindu@live.com>
2024-12-22 13:11:28 +0530

    disable export mode

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: "export",
+  /*output: "export",
   images: {
     unoptimized: true,
-  }
+  }*/
 };
 
 export default nextConfig;
